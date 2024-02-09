b009ca4cf454614d1fc07b0705c7564fa86dea5a
Georg <gwbootcamp24.google.com>
2024-02-09 21:25:43 +0100

    endlich die Navi grundlegend zum laufen gebracht

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-console.log('hi there');
+document.addEventListener("DOMContentLoaded", (event) => {
+    let toggler = document.querySelectorAll("[data-toggle-target]")
+  
+    if (toggler.length > 0) {
+      toggler.forEach(function (element) {
+        const target = element.getAttribute("data-toggle-target")
+        const allTargets = document.querySelectorAll('.subMenu')
+        const targets = Array.from( document.querySelectorAll(target))
+        const toggleClass = element.getAttribute("data-toggle-class")
+        
+        element.addEventListener("click", (event) => {
+          const allTargetsButTarget = Array.from( allTargets ).filter( ( node, index ) => !targets.includes(node))
+
+
+          allTargetsButTarget.forEach(function (e) {
+            console.log('allTargetsButTarget',e.classList);
+            console.log('allTargetsButTarget',e );
+            e.classList.remove(toggleClass)
+            console.log('allTargetsButTarget',e.classList);
+          })
+          targets.forEach(function (e) {
+            e.classList.toggle(toggleClass)
+
+        })
+        })
+      })
+    }
+
+
+  });
