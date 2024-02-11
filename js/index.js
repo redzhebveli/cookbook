f04ad8b78c893922856d92467f9c1990671f4e79
Georg <gwbootcamp24.google.com>
2024-02-11 15:35:56 +0100

    fetch page load

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         const toggleClass = element.getAttribute("data-toggle-class")
         
         element.addEventListener("click", (event) => {
+
           const allTargetsButTarget = Array.from( allTargets ).filter( ( node, index ) => !targets.includes(node))
 
 
@@ -25,6 +26,44 @@ document.addEventListener("DOMContentLoaded", (event) => {
         })
       })
     }
+const getPage = ((page) => {
 
+  let response =  fetch(page).then(response => response.text()).then(data => {
+    document.getElementById('ajaxHere').innerHTML = data;
+    return data 
+  }
+  );
+
+});
+
+
+  let subpages = document.querySelectorAll('.subMenu a');
+
+  subpages.forEach( (element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault();
+      const page = element.getAttribute('href') 
+      getPage(page);
+
+      // const myAsync = async () => {
+      //   const newPageHtml = await newPage;
+      //   console.log('###',newPageHtml);
+      //   // document.getElementById('ajaxHere').innerHTML = newPageHtml;
+      // };
+      // myAsync();
+
+      // const newPageHtml = () => {
+      //   newPage.then((a) => {
+      //     console.log(a);
+      //   });
+      // };
+      // newPageHtml();
+       
+    })
+  }
+  );
+
+// let text =  response.text(); // read response body as text
+// console.log(response.then(response => response.text()));
 
   });
