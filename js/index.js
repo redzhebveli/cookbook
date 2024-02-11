15c7f7390353da9befc8dbaa337089c3ebca930a
Georg <gwbootcamp24.google.com>
2024-02-11 19:48:57 +0100

    put it all together

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+  document.addEventListener("DOMContentLoaded", (event) => {
     let toggler = document.querySelectorAll("[data-toggle-target]")
   
     if (toggler.length > 0) {
@@ -26,44 +26,44 @@ document.addEventListener("DOMContentLoaded", (event) => {
         })
       })
     }
-const getPage = ((page) => {
+    const getPage = ((page) => {
+      const path = 'rezepte/ajax/';
+      let response =  fetch(path + page).then(response => response.text()).then(data => {
+        document.getElementById('ajaxHere').innerHTML = data;
+        return data 
+      }
+      );
 
-  let response =  fetch(page).then(response => response.text()).then(data => {
-    document.getElementById('ajaxHere').innerHTML = data;
-    return data 
-  }
-  );
-
-});
+    });
 
 
-  let subpages = document.querySelectorAll('.subMenu a');
+    let subpages = document.querySelectorAll('.subMenu a');
 
-  subpages.forEach( (element) => {
-    element.addEventListener('click', (event) => {
-      event.preventDefault();
-      const page = element.getAttribute('href') 
-      getPage(page);
+    subpages.forEach( (element) => {
+      element.addEventListener('click', (event) => {
+        event.preventDefault();
+        const page = element.getAttribute('href') 
+        getPage(page);
 
-      // const myAsync = async () => {
-      //   const newPageHtml = await newPage;
-      //   console.log('###',newPageHtml);
-      //   // document.getElementById('ajaxHere').innerHTML = newPageHtml;
-      // };
-      // myAsync();
+        // const myAsync = async () => {
+        //   const newPageHtml = await newPage;
+        //   console.log('###',newPageHtml);
+        //   // document.getElementById('ajaxHere').innerHTML = newPageHtml;
+        // };
+        // myAsync();
 
-      // const newPageHtml = () => {
-      //   newPage.then((a) => {
-      //     console.log(a);
-      //   });
-      // };
-      // newPageHtml();
-       
-    })
-  }
-  );
+        // const newPageHtml = () => {
+        //   newPage.then((a) => {
+        //     console.log(a);
+        //   });
+        // };
+        // newPageHtml();
+        
+      })
+    }
+    );
 
-// let text =  response.text(); // read response body as text
-// console.log(response.then(response => response.text()));
+  // let text =  response.text(); // read response body as text
+  // console.log(response.then(response => response.text()));
 
   });
